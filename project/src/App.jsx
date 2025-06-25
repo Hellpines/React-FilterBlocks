d97b1c417966c70c8175ad76d74220300b7cf66c
Ilya Tolstenkov <tolstenkov.ilya@mail.ru>
2025-06-25 20:19:13 +0300

    fixed bags

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,23 +1,17 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import MainPage from './Pages/MainPage/MainPage'
-<<<<<<< HEAD
 import VacancyPage from './Pages/VacancyPage/VacancyPage'
 import StarPage from './Pages/StarPage/StarPage'
-=======
 import AboutVacancy from './Pages/AboutVacancy/AboutVacancy'
->>>>>>> 844753582d0f5c9984448a39a81ec3024f37aa0b
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<MainPage/>}></Route>
-<<<<<<< HEAD
       <Route path='/basket' element={<StarPage/>}></Route>
       <Route path='/:id' element={<VacancyPage/>}></Route>
-=======
       <Route path='/about-vacancy' element={<AboutVacancy/>}></Route>
->>>>>>> 844753582d0f5c9984448a39a81ec3024f37aa0b
     </Routes>
   )
 }
--- project/src/Components/Header/Header.jsx
+++ project/src/Components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import style from './style.module.scss'
-<<<<<<< HEAD
 import img from './Union.svg';
 import {Link} from 'react-router-dom'
 function Header(){
@@ -19,25 +18,4 @@ function Header(){
     )
 }
 export default Header;
-=======
-import img from './Union.svg'
-function Header(){
-    return (
-        <div>
-            <div className={style.wrapper}>
-                <div className={style.content}>
-                    <div className={style.logo}>
-                        <img src={img}/>
-                        <h2>Jobored</h2>
-                    </div>
-                    <div className={style.links}>
-                        <p>Поиск Вакансий</p>
-                        <p>Избранное</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-export default Header
->>>>>>> 844753582d0f5c9984448a39a81ec3024f37aa0b
+
